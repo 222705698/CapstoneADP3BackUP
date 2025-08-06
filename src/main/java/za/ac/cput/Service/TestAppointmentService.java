d1baffbb3a3d2c265f31a9f8d4e50b12c5959d1f
Sinokholo <222705698@mycput.ac.za>
2025-08-07 01:35:59 +0200

    Added Services and Tests

---
+++ src/main/java/za/ac/cput/Service/TestAppointmentService.java
@@ -0,0 +1,39 @@
+package za.ac.cput.Service;
+
+import org.springframework.stereotype.Service;
+import za.ac.cput.Domain.bookings.TestAppointment;
+import za.ac.cput.Repository.TestAppointmentRepository;
+
+import java.util.Collections;
+import java.util.List;
+
+@Service
+public class TestAppointmentService implements ITestAppointmentService {
+
+    private final TestAppointmentRepository testAppointmentRepository;
+
+    public TestAppointmentService(TestAppointmentRepository testAppointmentRepository) {
+        this.testAppointmentRepository = testAppointmentRepository;
+    }
+
+    @Override
+    public TestAppointment create(TestAppointment testAppointment) {
+        return testAppointmentRepository.save(testAppointment);
+    }
+
+    @Override
+    public TestAppointment read(Long id) {
+        return testAppointmentRepository.findById(id).orElse(null); // Use Long here
+    }
+
+    @Override
+    public TestAppointment update(TestAppointment testAppointment) {
+        return testAppointmentRepository.save(testAppointment);
+    }
+
+
+    @Override
+    public List<TestAppointment> findByTestId(Long testAppointmentId) {
+        return Collections.singletonList(testAppointmentRepository.findByTestId(testAppointmentId));
+    }
+}
