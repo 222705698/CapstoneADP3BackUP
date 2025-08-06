67be163a97d8caaf61bb6be7cda24688fe26b998
Sibahle_Shange <222529571@mycput.ac.za>
2025-08-07 01:47:00 +0200

    Did my Repository classes

---
+++ src/main/java/za/ac/cput/Repository/VehicleDiscRepository.java
@@ -0,0 +1,8 @@
+package za.ac.cput.Repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+import za.ac.cput.Domain.bookings.VehicleDisc;
+@Repository
+public interface VehicleDiscRepository extends JpaRepository<VehicleDisc, Integer> {
+}
