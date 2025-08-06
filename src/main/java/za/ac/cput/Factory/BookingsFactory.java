116830a530973dd4ffad13ba17b25ee8299b9695
EmihleThole <164901595+EmihleThole@users.noreply.github.com>
2025-08-06 13:42:25 +0200

    bookings Factory

--- src/main/java/za/ac/cput/Factory/BookingsFactory.java
+++ src/main/java/za/ac/cput/Factory/BookingsFactory.java
@@ -1,29 +1,28 @@
 package za.ac.cput.Factory;
 
 import za.ac.cput.Domain.bookings.Bookings;
+import za.ac.cput.Domain.bookings.Test;
+import za.ac.cput.Domain.bookings.VehicleDisc;
 import za.ac.cput.Util.Helper;
 
 import java.time.LocalDate;
 
 public class BookingsFactory {
 
-public static Bookings createBookings(  LocalDate bookingDate, String bookingType, Double bookingAmount){
+    public static Bookings createBookingsFactory(String bookingType, LocalDate bookingDate, VehicleDisc vehicleDisc, Test test) {
+        int bookingId = Helper.generateUniqueId();
 
+        if (Helper.isNullOrEmpty(bookingType) || bookingDate == null || vehicleDisc == null || test == null) {
+            return null;
+        }
+        return new Bookings.Builder()
+                .setBookingId(bookingId)
+                .setBooktype(Bookings.Booktype.valueOf(bookingType))
+                .setBookingDate(bookingDate)
+                .setVehicleDisc(vehicleDisc)
+                .setTest(test)
+                .build();
+    }
 
-    int bookingId = Helper.generateUniqueId();
-    if (bookingAmount == null || bookingAmount <= 0)
-        return null;
-    if(Helper.isNullOrEmpty(bookingType))
-        return null;
-
-    if (Helper.isDateInPast(bookingDate))
-        return null;
-
-    return new Bookings.Builder()
-            .setBookingId(bookingId)
-            .setBookingDate(bookingDate)
-            .setBookingType(bookingType)
-            .setBookingAmount(bookingAmount)
-            .build();
 }
-}
+
