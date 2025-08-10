86648ab8880fc3b1fd9ded12d9acac6ac8c7452e
Sibahle_Shange <222529571@mycput.ac.za>
2025-08-11 01:36:00 +0200

    created my controller classes

--- src/main/java/za/ac/cput/Controller/Booking/VehicleDiscController.java
+++ src/main/java/za/ac/cput/Controller/Booking/VehicleDiscController.java
@@ -1,4 +1,44 @@
 package za.ac.cput.Controller.Booking;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+import za.ac.cput.Domain.bookings.VehicleDisc;
+import za.ac.cput.Service.impl.VehicleDiscService;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/vehicledisc")
 public class VehicleDiscController {
+    private VehicleDiscService vehicleDiscService;
+
+    @Autowired
+    public VehicleDiscController(VehicleDiscService vehicleDiscService) {
+        this.vehicleDiscService = vehicleDiscService;
+    }
+
+    @PostMapping("/create")
+    public VehicleDisc create(@RequestBody VehicleDisc vehicleDisc) {
+        return vehicleDiscService.create(vehicleDisc);
+    }
+
+    @GetMapping("/read/{discId}")
+
+    public VehicleDisc read(@PathVariable Long discId) {
+        return vehicleDiscService.read(discId);
+    }
+
+    @PutMapping("/update")
+    public VehicleDisc update(@RequestBody VehicleDisc vehicleDisc) {
+        return vehicleDiscService.update(vehicleDisc);
+    }
+    @DeleteMapping("/delete/{discId}")
+    public void delete(@PathVariable Long discId) {
+        vehicleDiscService.delete(discId);
+    }
+    @GetMapping("/getAll")
+    public List<VehicleDisc> getAll() {
+        return vehicleDiscService.getAll();
+    }
+
 }
--- src/main/java/za/ac/cput/Controller/Registration/VehicleController.java
+++ src/main/java/za/ac/cput/Controller/Registration/VehicleController.java
@@ -1,4 +1,42 @@
 package za.ac.cput.Controller.Registration;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+import za.ac.cput.Domain.Registrations.Vehicle;
+import za.ac.cput.Service.impl.VehicleService;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/vehicle")
 public class VehicleController {
+    private VehicleService vehicleService;
+
+    @Autowired
+    public VehicleController(VehicleService vehicleService) {
+        this.vehicleService = vehicleService;
+    }
+    @PostMapping("/create")
+    public Vehicle create(@RequestBody Vehicle vehicle) {
+        return vehicleService.create(vehicle);
+    }
+
+    @GetMapping("/read/{VehicleID}")
+    public Vehicle read(@PathVariable int VehicleID) {
+        return vehicleService.read(VehicleID);
+    }
+
+    @PutMapping("/update")
+    public Vehicle update(@RequestBody Vehicle vehicle) {
+        return vehicleService.update(vehicle);
+    }
+
+    @DeleteMapping("/delete/{VehicleID}")
+    public void delete(@PathVariable int VehicleID) {
+        vehicleService.delete(VehicleID);
+    }
+    @GetMapping("/getAll")
+    public List<Vehicle> getAll() {
+        return vehicleService.getAll();
+    }
 }
